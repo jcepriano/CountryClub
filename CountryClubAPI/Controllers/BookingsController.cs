14af2044e9c4ee2de96c077a4b29d49c2dc38c4a
jcepriano <jamescepriano@gmail.com>
2023-10-09 14:08:15 -0700

    deleted comments. I don't know if my json requests are wrong or if my code is wrong. my create booking action is not working.

--- CountryClubAPI/Controllers/BookingsController.cs
+++ CountryClubAPI/Controllers/BookingsController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CountryClubAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/bookings")]
     [ApiController]
     public class BookingsController : ControllerBase
     {
@@ -25,15 +25,15 @@ namespace CountryClubAPI.Controllers
         [HttpPost]
         public ActionResult CreateBooking(Booking booking)
         {
-            if (!ModelState.IsValid)
-            {
-                Response.StatusCode = 400;
-                return BadRequest();
-            }
+            //if (!ModelState.IsValid)
+            //{
+            //    Response.StatusCode = 400;
+            //    return BadRequest();
+            //}
 
             _context.Bookings.Add(booking);
             _context.SaveChanges();
-            Response.StatusCode = 201;
+            //Response.StatusCode = 201;
 
             return new JsonResult(booking);
 
